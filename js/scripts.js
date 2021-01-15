3c565cd67d6f811dc6acb8d90279c29da21bcfeb
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-15 14:19:14 -0800

    Set up functionality for combinations that aren't accounted for

--- js/scripts.js
+++ js/scripts.js
@@ -13,31 +13,42 @@ $(document).ready(function(){
     const answer5 = parseInt($('select#question5').val(), 10)
     // 1:software, 2:game, 3:app, 4:no like
     
+    let ifTestVar;
+    let outputTest;
+
     if(isNaN(answer1) || isNaN(answer2) || isNaN(answer3) ||isNaN(answer4) || isNaN(answer5)){
       $('div#fail').text("Please don't leave any selections blank");
-    } else {
+      ifTestVar = "";
+    } else if(ifTestVar === undefined){
       if((answer5 === 4 || (answer1 === 4 && answer2 === 4) || (answer2 === 4 && answer3 === 4 && answer4 === 4))){
         $('span#output').text('It sounds like your interests may be geared away from working with computers! That\'s ok! Take some more time to explore coding!');
         $('div#hide').hide();
         $('div#hidden').show();
+        outputTest = ('span#output').val();
         // JavaScript Branch
-      } else if(answer1 === 1 && answer2 === 1 && (answer3 === 2 || 3) && (answer4 === 1 || 3) && answer5 === 1) {
+      } else if((answer1 === 1 || 4) && answer2 === 1 && (answer3 === 2 || 3) && (answer4 === 1 || 3) && answer5 === 1) {
         $('span#output').text('JavaScript sounds like a great language for a free spirited and comfy person like you!');
         $('div#hide').hide();
         $('div#hidden').show();
+        outputTest = ('span#output').val();
         // C# Branch
       } else if((answer1 === 1 || 2) && answer2 === 2 && (answer3 === 1 || 2 || 3) && (answer4 === 1 || 2 || 3) && answer5 === 2) {
         $('span#output').text('C# sounds like a great language for a focused/disciplined and stylish person such as yourself!');
         $('div#hide').hide();
         $('div#hidden').show();
-      } else if((answer1 === 1 || 2) && answer2 === 3 && (answer3 === 2 || 3) && (answer4 === 1 || 3) && answer5 === 3){
+        outputTest = ('span#output').val();
+      } else if((answer1 === 1 || 3) && answer2 === 3 && (answer3 === 2 || 3) && (answer4 === 1 || 3) && answer5 === 3){
         $('span#output').text('Python is a great language for a comfy and logical person, sounds a lot like you!');
         $('div#hide').hide();
         $('div#hidden').show();
+        outputTest = ('span#output').val();
+      } else {
+        if(outputTest === undefined && ){
+          $('span#output').text('You may be too complicated and outdoorsy for me to figure out! JavaScript, C# and Python may all be great places to start!');
+          $('div#hide').hide();
+          $('div#hidden').show();
+          }
       }
     }
-
-    
-    
   });
 });
